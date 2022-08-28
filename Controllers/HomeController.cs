b017b5ac497482f659804ec123742611bc94e824
pandoraciel <86033350+pandoraciel@users.noreply.github.com>
2022-08-28 11:20:01 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SimpleSampleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleSampleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            if(String.IsNullOrEmpty(HttpContext.Session.GetString("username")))
+            {
+                return View("Login");
+            }
+
+            var DataWeather = Get();
+            return View(DataWeather);          
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("username");
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Login(string username, string password)
+        {
+            if (username != null && password != null && username.Equals("acc1@mail.com") && password.Equals("123"))
+            {
+                HttpContext.Session.SetString("username", username.Split("@")[0]);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.error = "Invalid Account or Password";
+                return View("Login");
+            }
+        }
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 30).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
